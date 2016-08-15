ae8b8a2d8eb7415ab7a737c0a0263264fbc2b37e
mingyuchoo <mingyuchoo@gmail.com>
2016-08-15 11:28:28 +0900

    modified

--- myartworks/settings.py
+++ myartworks/settings.py
@@ -140,10 +140,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, 'static'),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'static'),
+# ]
 
 # Media files
 MEDIA_URL = '/media/'
