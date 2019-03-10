02ecfcf62226b7b9ed5c8e4576d0efa7de5e29d0
mingyuchoo <mingyuchoo@gmail.com>
2019-03-10 12:54:42 +0900

    change Django version to 2.1.7

--- myartworks/settings.py
+++ myartworks/settings.py
@@ -124,13 +124,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
 LANGUAGE_CODE = 'ko-kr'
-
 TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
